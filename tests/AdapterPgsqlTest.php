08ecf432743d8a2f2db8042f484cd3214f2f68a0
basakest <ab1652759879@gmail.com>
2021-08-09 20:59:30 +0800

    feat: use 'Github Actions' to run tests and auto release

--- tests/AdapterPgsqlTest.php
+++ tests/AdapterPgsqlTest.php
@@ -8,10 +8,10 @@ class AdapterPgsqlTest extends AdapterTest
     {
         $this->config = [
             'driver' => 'Pdo_Pgsql',
-            'hostname' => $this->env('DB_PORT', '127.0.0.1'),
+            'hostname' => $this->env('DB_HOST', '127.0.0.1'),
             'database' => $this->env('DB_DATABASE', 'casbin'),
             'username' => $this->env('DB_USERNAME', 'postgres'),
-            'password' => $this->env('DB_PASSWORD', ''),
+            'password' => $this->env('DB_PASSWORD', 'postgres'),
             'port' => $this->env('DB_PORT', 5432),
         ];
     }
